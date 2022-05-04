c5c31b9baca0c667b9a3ac7bff0dc261187118ea
zpengineer <clickprime94@gmail.com>
2022-05-04 21:00:29 +0300

    Added start file project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import Container from './Container/Container';
+import Feedback from './Feedback/Feedback';
+import Controls from './Controls/Controls';
+import Notification from './Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = e => {
+    const currentBtn = e.target.value;
+
+    this.setState(prevState => {
+      return {
+        [currentBtn]: prevState[currentBtn] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback() {
+    const feedbackValues = Object.values(this.state);
+    return feedbackValues.reduce((prevValue, num) => prevValue + num, 0);
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+
+    const positiveFeedback = (good / total) * 100;
+    return Math.round(positiveFeedback);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const feedbackKey = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+
+    return (
+      <Container>
+        <h1>Please leave feedback</h1>
+        <Section>
+          <Controls keys={feedbackKey} onIncrement={this.handleFeedback} />
+        </Section>
+
+        <h2>Statistic</h2>
+        <Section>
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Feedback
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positiveFeedback={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Controls.module.css';
+
+const Controls = ({ keys, onIncrement }) => {
+  return (
+    <ul className={styles.list}>
+      {keys.map((key, index) => (
+        <li key={index} className={styles.item}>
+          <button className={styles.button} value={key} onClick={onIncrement}>
+            {key}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+Controls.propType = {
+  keys: PropTypes.array.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+};
+
+export default Controls;
